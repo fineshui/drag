006b4e3ef402fd0de4d418284377f6ebe79a90a4
fineshui <wangshuichang528@163.com>
2018-03-02 15:30:47 +0800

    change drag.js from master

--- drag.js
+++ drag.js
@@ -5,8 +5,15 @@ function drag(id)
 	var obj = document.getElementById(id);
 	var disX = 0;
 	var disY = 0;
-	obj.onmousedown = function(ev){
+	obj.onmousedown = function(){
 		disX = ev.pageX - obj.offsetLeft;
 		disY = ev.pageY - obj.offsetTop;
+		document.onmousemove = function(ev)
+		{
+			obj.style.left = ev.pageX - disX + 'px';
+			obj.style.top = ev.pageY - disY + 'px';
+		};
 	}
+	obj.onmousemove = function(){};
+	obj.onmouseup = function(){};
 }
